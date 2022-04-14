ee49ef7c8bbff1733b2c6ed77e486199fc8aabd9
Sooyeon Im <isyqwer1@gmail.com>
2022-04-14 16:32:04 +0900

    Modify: users application

--- students/sooyeon/users/models.py
+++ students/sooyeon/users/models.py
@@ -4,9 +4,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300, unique=True)
     password = models.CharField(max_length=300)
-    contact = models.CharField(max_length=300)
+    contact = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = 'users'
