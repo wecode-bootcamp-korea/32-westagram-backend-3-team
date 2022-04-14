4b2b98eaec0ec2235352b0e3f8f0acb4e1dbd31f
MinseokChoi <alex0316@gmail.com>
2022-04-14 16:37:18 +0900

    Docs: Models.py_class update

--- students/minseok/westagram/users/models.py
+++ students/minseok/westagram/users/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name  = models.CharField(max_length=20)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    Cellphone = models.CharField(max_length=11))
+   user_name  = models.CharField(max_length=20)
+   user_email = models.CharField(unique=True)
+   user_password = models.CharField(max_length=100)
+   user_phone_number = models.CharField(max_length=30)
+
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
