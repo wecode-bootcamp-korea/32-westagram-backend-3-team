1ab567ec67ed83ef0077c86d865879119d2c11ef
MinseokChoi <alex0316@gmail.com>
2022-04-14 17:18:15 +0900

    Docs: Models.py update, Docs: settings.py_installed app update

--- students/minseok/westagram/users/models.py
+++ students/minseok/westagram/users/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-   user_name  = models.CharField(max_length=20)
-   user_email = models.CharField(unique=True)
-   user_password = models.CharField(max_length=100)
-   user_phone_number = models.CharField(max_length=30)
-
+   name  = models.CharField(max_length=20)
+   email = models.CharField(unique=True)
+   password = models.CharField(max_length=100)
+   phone_number = models.CharField(max_length=30)
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
 
