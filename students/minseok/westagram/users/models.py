987d5fc6350d2504d40fbaa1c73796201be64e2f
MinseokChoi <alex0316@gmail.com>
2022-04-14 16:15:10 +0900

    Docs: Moels.py update

--- students/minseok/westagram/users/models.py
+++ students/minseok/westagram/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name  = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    Cellphone = models.IntegerField(Null=True)
+    Cellphone = models.CharField(max_length=11))
 
     class Meta:
         db_table = 'users'
