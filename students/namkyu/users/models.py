9cd9ba5949a93f1e8d00399dc94996250d352122
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-14 17:28:01 +0900

    ADD:created_at, updated_at, Edit:email. phone_number

--- students/namkyu/users/models.py
+++ students/namkyu/users/models.py
@@ -1,14 +1,12 @@
-from platform import release
-from pyexpat import model
-from tkinter import CASCADE
-from turtle import ondrag
 from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField()
+    email = models.CharField(unique=True,max_length=100)
     password = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=13)
+    phone_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
--- students/namkyu/westargram/settings.py
+++ students/namkyu/westargram/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     # 'django.contrib.admin',
     # 'django.contrib.auth',
+    'users',
     'corsheaders',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
