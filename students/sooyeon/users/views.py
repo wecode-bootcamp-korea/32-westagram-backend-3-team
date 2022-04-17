2e95a0e252290ba5d1948a0eef1e18c9b20d48bd
Sooyeon Im <isyqwer1@gmail.com>
2022-04-18 00:06:26 +0900

    Modify: sign up feature

--- students/sooyeon/users/views.py
+++ students/sooyeon/users/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from tkinter import NONE
 
-# Create your views here.
 import json
 
 from django.http import JsonResponse
@@ -19,21 +17,26 @@ class SignUpView(View):
             email = data['email']
             password = data['password']
             contact = data['contact']
-            
+        
+            email_regex = '([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
+            password_regex = r"^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z0-9$@$!%*#?&].{8,}$"
+        
+            if re.match(email_regex, email) is None:
+                return JsonResponse({'message': 'EMAIL_INVALIDATION'}, status = 400)
+        
+            if re.match(password_regex, password) is None:
+                return JsonResponse({'message': 'PASSWORD_INVALIDATION'}, status = 400)
+        
+            User.objects.create(
+                name = data['name'],
+                email = data['email'],
+                password = data['password'],
+                contact = data['contact']
+            )
+        
+            return JsonResponse({'message':'SUCCESS'}, status = 201)
+        
         except:
             return JsonResponse({"message": "KEY_ERROR"}, status = 400)
         
-        if re.match('([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email) is None:
-            return JsonResponse({'message': 'EMAIL_INVALIDATION'}, status = 400)
         
-        if re.match(r"^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z0-9$@$!%*#?&].{8,}$", password) is None:
-            return JsonResponse({'message': 'PASSWORD_INVALIDATION'}, status = 400)
-        
-        User.objects.create(
-            name = data['name'],
-            email = data['email'],
-            password = data['password'],
-            contact = data['contact']
-        )
-        
-        return JsonResponse({'message':'SUCCESS'}, status = 201)
