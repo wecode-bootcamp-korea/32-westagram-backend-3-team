f213cee0c69ba00424f15e1b087f19d4d5e8a115
YoungbinKim <colock123@gmail.com>
2022-04-18 14:50:40 +0900

    sign_upfinished 0.3

--- students/youngbin/users/validation.py
+++ students/youngbin/users/validation.py
@@ -1,21 +1,22 @@
 import re
-from django.http import JsonResponse
+from django.http            import JsonResponse
+from django.core.exceptions import ValidationError
 
+REGEX_PASSWORD = '^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,}$'
+REGEX_EMAIL = r'^\w+[\.\w]*@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$'
+REGEX_PHONE_NUMBER = r'^\d{3}-\d{3,4}-\d{4}$'
 
 def validate_password(password):
-    REGEX_PASSWORD = '^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,}$'
-    if re.search(REGEX_PASSWORD,password) is None:
-        return JsonResponse({"message":"Incorrect password format"},status=401)
+    if not re.search(REGEX_PASSWORD,password):
+        raise ValidationError('INVALID_PASSWORD')
     return password
 
 def validate_email(email):
-    REGEX_EMAIL = r'^\w+[\.\w]*@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$'
-    if re.search(REGEX_EMAIL,email) is None:
-        return JsonResponse({"message":"Incorrect email format"},status=400)
+    if not re.search(REGEX_EMAIL,email):
+        raise ValidationError('INVALID_EMAIL_ADDRESS')
     return email
 
 def validate_phone_number(phone_number):
-    REGEX_PHONE_NUMBER = r'^\d{3}-\d{3,4}-\d{4}$'
-    if re.search(REGEX_PHONE_NUMBER,phone_number) is None:
-        return JsonResponse({"message":"Incorrect phone_number format"},status=409)
+    if not re.search(REGEX_PHONE_NUMBER,phone_number):
+        raise ValidationError('INVALID_PHONE_NUMBER')
     return phone_number
--- students/youngbin/users/views.py
+++ students/youngbin/users/views.py
@@ -1,9 +1,9 @@
 import json
-
-from django.http  import JsonResponse
-from django.views import View
-from .models      import User
-from .validation  import (
+from django.core.exceptions import ValidationError
+from django.http            import JsonResponse
+from django.views           import View
+from .models                import User
+from .validation            import (
     validate_password,
     validate_email,
     validate_phone_number)
@@ -18,30 +18,26 @@ class UserView(View):
             new_name         = data['name']
             new_phone_number = data['phone_number']
 
-
-            validated_pw = validate_password(new_password)
-            if isinstance(validated_pw,JsonResponse):
-                return validated_pw
-            
-            validated_email = validate_email(new_email)
-            if isinstance(validated_email,JsonResponse):
-                return validated_email
-
-            validated_phone_number = validate_phone_number(new_phone_number)
-            if isinstance(validated_phone_number,JsonResponse):
-                return validated_phone_number
-            
-
-            if User.objects.filter(email=new_email).exists():
-                return JsonResponse({"message":"ALREADY_EXISTED_EMAIL"},status=409)
-
-            User.objects.create(
-                name         = new_name,
-                password     = validated_pw,
-                email        = validated_email,
-                phone_number = validated_phone_number,
-            )
-            return JsonResponse({'messasge':'created'}, status=201)
-
         except:
             return JsonResponse({"message":"KEY_ERROR"},status=400)
+
+        try:
+            validated_pw = validate_password(new_password)
+            validated_email = validate_email(new_email)
+            validated_phone_number = validate_phone_number(new_phone_number)
+        # except ValidationError as error:
+        #     JsonResponse({"message": error.messages}, status=409)
+        except ValidationError as error:
+            return JsonResponse({"message": error.messages}, status=409)
+
+
+        if User.objects.filter(email=new_email).exists():
+            return JsonResponse({"message":"ALREADY_EXISTED_EMAIL"},status=409)
+
+        User.objects.create(
+            name         = new_name,
+            password     = validated_pw,
+            email        = validated_email,
+            phone_number = validated_phone_number,
+        )
+        return JsonResponse({'messasge':'created'}, status=201)
