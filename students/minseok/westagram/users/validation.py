44a3324153bcbb3d8663b226f632437579efff36
MinseokChoi <alex0316@gmail.com>
2022-04-18 22:15:44 +0900

    feat: update views.py

--- students/minseok/westagram/users/validation.py
+++ students/minseok/westagram/users/validation.py
@@ -3,12 +3,12 @@ import re
 from users.models import User
 
 class Validation:
-      def email_validate(value):
-          REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
-          if not re.match(REGEX_EMAIL, 'email'):
-             return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
-      def password_validate(value):
-	  REGEX_PASSWORD = '\S{8,25}'         
-	  if not re.match(REGEX_PASSWORD, 'password'):
-             return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
-                
+    def email_validate(value):
+        REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
+        if not re.match(REGEX_EMAIL, 'email'):
+            return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
+    def password_validate(value):
+	REGEX_PASSWORD = '\S{8,25}'         
+	if not re.match(REGEX_PASSWORD, 'password'):
+            return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
+    
--- students/minseok/westagram/users/views.py
+++ students/minseok/westagram/users/views.py
@@ -35,3 +35,18 @@ class SignUpView(View):
 
           except KeyError:
               return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+
+class LoginView(View):
+      def post(self, request):
+	  try:
+	      data     = json.loads(request.body)
+	      email    = data['email']
+	      password = data['password']
+
+	      if User.objects.filter(email=email, password=password).exists():
+	          return JsonResponse({"message": "SUCCESS"}, status code 200)
+	      else:
+	          return JsonResponse({"message":"INVALID_USER."},status=401)
+
+	  except KeyError:
+	      return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
