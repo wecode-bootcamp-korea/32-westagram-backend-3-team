c989bd26c99d5747f72410a9b293a6bc8370aa6a
MinseokChoi <alex0316@gmail.com>
2022-04-19 16:40:47 +0900

    feat: apply bcrypt

--- students/minseok/westagram/users/views.py
+++ students/minseok/westagram/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-import json, re
+import json, re, bcrypt
 
 from django.http      import JsonResponse, HttpResponse
 from django.views     import View
@@ -24,11 +24,13 @@ class SignUpView(View):
 
               if User.objects.filter(email=email).exists():
                   return JsonResponse({'message': 'ALREADY_EXISTS'}, status = 400)
-                      
+              
+              password_bcrypt = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
               User.objects.create(
                   name = name,
                   email = email, 
-                  password = password, 
+                  password = password_bcrypt, 
                   phone_number = phone_number
                   )
               return JsonResponse({'message': 'SUCCESS'}, status = 201)
