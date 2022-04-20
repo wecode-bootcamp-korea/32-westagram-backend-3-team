9b7db4c54fd19b8bc28e32510c6536d9f1091b66
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-04-20 10:34:22 +0900

    Edit : email variable in login function

--- students/namkyu/users/views.py
+++ students/namkyu/users/views.py
@@ -18,28 +18,29 @@ class RegisterView(View):
     def post(self, request):
         try : 
             data             = json.loads(request.body)
+            entered_email    = data['email']
             entered_password = data['password']
             hashed_password  = bcrypt.hashpw(entered_password.encode('utf-8'), bcrypt.gensalt()).decode("utf-8")
 
-            if check_email_validation(data['email']) == None:
+            if check_email_validation(entered_email) == None:
                 return JsonResponse({'MESSAGE' : "Email validation error"},    status = 400)
 
             if check_password_validation(entered_password) == None:
                 return JsonResponse({'MESSAGE' : "Password validation error"}, status = 400)
                 
-            if User.objects.filter(email = data["email"]).exists():
+            if User.objects.filter(email = entered_email ).exists():
                 return JsonResponse({'MESSAGE' : 'Email already Exists'},      status = 400)
         
             User.objects.create(
                 name         = data['name'],
-                email        = data['email'],
+                email        = entered_email,
                 password     = hashed_password,
                 phone_number = data['phone_number'],
             )
-            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status = 201)
+            return JsonResponse({'MESSAGE' : 'SUCCESS'},                       status = 201)
 
         except KeyError :
-            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'},                     status = 400)
             
 class LoginView(View):
     def post(self, request):
@@ -51,7 +52,7 @@ class LoginView(View):
             if not User.objects.filter(email=entered_email, password=entered_password).exists(): 
                 return JsonResponse({'MESSAGE' : 'INVALID_USER'}, status = 401)
 
-            return JsonResponse({'MESSAGE' : 'LOGIN SUCCESS'}, status = 200)
+            return JsonResponse({'MESSAGE' : 'LOGIN SUCCESS'},    status = 200)
 
         except KeyError: 
-            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'},        status = 400)
